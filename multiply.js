acac0e6271862f0e7b36ffa1860938b5a26184de
lulenuru <lulenuru21@gmail.com>
2025-03-30 10:01:12 +0300

    add second test

--- multiply.js
+++ multiply.js
@@ -5,5 +5,5 @@
 
 export default function (num_one, num_two) {
     num_one * num_two
-    return 1;
+    return 4;
 }
--- test/test.js
+++ test/test.js
@@ -3,7 +3,11 @@ import {describe, it} from "node:test"
 import multiply from "../multiply.js";
 
 describe ('test to see if you can multiply well', () => {
-    it('test if 1 * 1 = 1', () => {
+    it.skip('test if 1 * 1 = 1', () => {
         assert.equal(multiply(1,1),1);
     })
+    it('test if 2 * 2 = 4', () => {
+        assert.equal(multiply(2,2),4);
+    })
 })
+
